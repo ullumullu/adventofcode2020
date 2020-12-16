9acdc2bd2feb28f75457121fb0e4f724bd6fc8b3
Sven Sterbling <sven@sterbling.com>
2020-12-16 16:41:19 +0100

    Solving day 1 challenge
    More info: https://adventofcode.com/2020/day/1
    
    Simple python based solutions mostly using combinations to get all combinations
    and return the first that equals to 2020.

---
+++ challenges/day1.py
@@ -0,0 +1,15 @@
+from math import prod
+from typing import List
+from itertools import combinations
+
+
+def find_entries(report: List[int], *, sum_equals: int, elements: int) -> int:
+    """This is the first challenge of day1. 
+
+    Specifically, they need you to find the two (or three) entries that sum to 2020
+    and then multiply those two (or three) numbers together.
+    """
+    for combination in combinations(report, elements):
+        if sum(combination) == sum_equals:
+            return prod(combination)
+    return -1
---
+++ tests/test_day1.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+from challenges.day1 import find_entries
+from typing import List
+
+
+def _read_input() -> List[int]:
+    """Read the input file. Every line represents an integer value."""
+    input_report_repair = []
+    current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+    image_path = current_path / "resources" / "day1_puzzle_input.txt"
+    with image_path.open("r", encoding="utf-8") as input_file:
+        for line in input_file.readlines():
+            input_report_repair.append(int(line.strip()))
+    return input_report_repair
+
+
+def test_sample_input_part1():
+    sample_input = [1721, 979, 366, 299, 675, 1456]
+    expected = 514579
+    result = find_entries(sample_input, sum_equals=2020, elements=2)
+    assert result == expected
+
+
+def test_puzzle_input_part1():
+    input_report_repair = _read_input()
+    result = find_entries(input_report_repair, sum_equals=2020, elements=2)
+    print(f"Result: {result}")
+    assert result == 935419
+
+
+def test_sample_input_part2():
+    sample_input = [1721, 979, 366, 299, 675, 1456]
+    expected = 241861950
+    result = find_entries(sample_input, sum_equals=2020, elements=3)
+    assert result == expected
+
+
+def test_puzzle_input_part2():
+    input_report_repair = _read_input()
+    result = find_entries(input_report_repair, sum_equals=2020, elements=3)
+    print(f"Result: {result}")
+    assert result == 49880012
